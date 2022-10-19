3aea9939bdc673a1ab78a1813f77d7bae595b017
Samuel Cox <samc1253@gmail.com>
2022-10-19 11:12:26 -0600

    Allow first nickname test to pass

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_reader :name, :monthly_cost, :contract_length
+  attr_reader :name, :monthly_cost, :contract_length, :nickname
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @nickname = nil
   end
 
   def first_name
