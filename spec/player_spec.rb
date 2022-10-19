94438eb0fdb570a7b8a6ea99c9ba963386c5cdf5
Samuel Cox <samc1253@gmail.com>
2022-10-19 11:10:49 -0600

    Add first test for nickname

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,11 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it 'has no nickname by default' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq nil
+  end
+
 end
