0a2f31b03958a27c912467f08de867fe11cb805d
Samuel Cox <samc1253@gmail.com>
2022-10-19 11:32:31 -0600

    Add test for adding players to the roster

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe Team do
 
     expect(team.player_count).to eq(0)
   end
+
+  it 'can add players to the roster' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Clayton Kershaw" , 1416666, 12)
+    player_2 = Player.new("Mookie Betts" , 2386363, 132)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+
+    expect(team.roster).to eq([player_1, player_2])
+    
+    expect(team.player_count).to eq(2)
+  end
 end
